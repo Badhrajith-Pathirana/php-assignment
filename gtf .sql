dbc72a906662dca41a82df6b0125953790c9dd20
Badhrajith-Pathirana <badhrajith.pathirana@gmail.com>
2019-01-10 12:04:25 +0530

    Added database backup

--- gtf .sql
+++ gtf .sql
@@ -1,13 +1,15 @@
 -- phpMyAdmin SQL Dump
--- version 4.5.1
--- http://www.phpmyadmin.net
+-- version 4.8.1
+-- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Jan 01, 2019 at 05:49 PM
--- Server version: 10.1.16-MariaDB
--- PHP Version: 7.0.9
+-- Host: localhost
+-- Generation Time: Jan 10, 2019 at 07:31 AM
+-- Server version: 10.1.33-MariaDB
+-- PHP Version: 7.2.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -23,6 +25,18 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `article`
+--
+
+CREATE TABLE `article` (
+  `articleId` int(11) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `paragraph` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `locations`
 --
 
@@ -31,8 +45,18 @@ CREATE TABLE `locations` (
   `lat` float(10,6) NOT NULL,
   `lng` float(10,6) NOT NULL,
   `description` varchar(250) NOT NULL,
+  `imagePath` varchar(255) NOT NULL,
   `loc_status` tinyint(1) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `locations`
+--
+
+INSERT INTO `locations` (`locID`, `lat`, `lng`, `description`, `imagePath`, `loc_status`) VALUES
+(1, 6.939710, 79.877419, 'fsdds', 'ffff', NULL),
+(2, 6.945504, 79.897507, 'xakx', 'ffff', 0),
+(3, 6.935620, 79.892357, 'fuhfufh', 'ffff', 0);
 
 -- --------------------------------------------------------
 
@@ -64,11 +88,23 @@ INSERT INTO `users` (`id`, `username`, `email`, `password`) VALUES
 (10, '', '', 'd41d8cd98f00b204e9800998ecf8427e'),
 (11, '', '', 'd41d8cd98f00b204e9800998ecf8427e'),
 (12, '', '', 'd41d8cd98f00b204e9800998ecf8427e'),
-(13, 'ewfjhwejf', 'chinthani96@gmal.com', '202cb962ac59075b964b07152d234b70');
+(13, 'ewfjhwejf', 'chinthani96@gmal.com', '202cb962ac59075b964b07152d234b70'),
+(14, '', '', 'd41d8cd98f00b204e9800998ecf8427e'),
+(15, '', '', 'd41d8cd98f00b204e9800998ecf8427e'),
+(16, '', '', 'd41d8cd98f00b204e9800998ecf8427e'),
+(17, '', '', 'd41d8cd98f00b204e9800998ecf8427e'),
+(18, '', '', 'd41d8cd98f00b204e9800998ecf8427e'),
+(19, 'Badhrajith', 'badhrajith.pathirana@gmail.com', 'f925916e2754e5e03f75dd58a5733251');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `article`
+--
+ALTER TABLE `article`
+  ADD PRIMARY KEY (`articleId`);
 
 --
 -- Indexes for table `locations`
@@ -87,15 +123,24 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `article`
+--
+ALTER TABLE `article`
+  MODIFY `articleId` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `locations`
 --
 ALTER TABLE `locations`
-  MODIFY `locID` int(10) NOT NULL AUTO_INCREMENT;
+  MODIFY `locID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
